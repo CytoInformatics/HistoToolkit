649cc816d1d5b02153c994304b3d47dd81b7c728
MasonMcGough <mcgough.mason@gmail.com>
2018-03-02 15:59:20 -0500

    Made some changes to prepare for including PythonHelpers library (NOT stable);

--- app/tools/histotoolkit.py
+++ app/tools/histotoolkit.py
@@ -1,8 +1,13 @@
-def test(data):
-    return data
+import os
 
+def count_data_types(data, folder):
+    
+    return {'data-type': ext}
+
+
+# TESTING ONLY
 def test_mult(data, arg1):
-    return data * arg1
+    return {'data': data * arg1}
 
 def test_power(data, arg1):
-    return data**arg1
+    return {'data': data**arg1}
--- app/views.py
+++ app/views.py
@@ -1,18 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
 from .tools import histotoolkit as htk
+import os
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+data_folder = '/home/masonmcgough/Workspace/HistoToolkit/app/test'
 
-@app.route('/test')
-def test():
-    ops_names = ['test_mult', 'test_power']
-    ops_params = [
-        {'arg1': 3},
-        {'arg1': 2}
-    ]
+def _call_ops(data, ops_names, ops_params):
     ops = [
         {'op': getattr(htk, op_name),
          'op_name': op_name,
@@ -20,14 +13,45 @@ def test():
         } for op_name, params in zip(ops_names, ops_params)
     ]
 
-    val = 2
+    ops_output = []
     for op in ops:
-        val = op['op'](val, **op['params'])
+        op_output = op['op'](data, **op['params'])
+        ops_output.append(op_output)
 
-    return "Result: {0}".format(val)
+        if 'data' in op_output:
+            data = op_output['data']
 
-@app.route('/data-step', methods=['POST'])
+    return data, ops_output
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data-step', methods=['GET', 'POST'])
 def data_step():
     # ops_names = request.form['ops_names']
     # ops_params = request.form['ops_params']
-    return None
+    ops_names = ['count_data_types']
+    ops_params = [
+        {'folder': data_folder}
+    ]
+
+    # return htk.get_data_type(ops_names, **)
+    # return _call_ops()
+    img, ops_output = _call_ops(None, ops_names, ops_params)
+    return jsonify(ops_output)
+
+
+# TESTING ONLY
+@app.route('/test')
+def test():
+    ops_names = ['test_mult', 'test_power']
+    ops_params = [
+        {'arg1': 3},
+        {'arg1': 2}
+    ]
+
+    val = 2
+    val, ops_output = _call_ops(val, ops_names, ops_params)
+
+    return "Result: {0}<br \>{1}".format(val, ops_output)
