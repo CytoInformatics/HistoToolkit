9b79d8ff1e3d8a9e17b8a3b2fa4307d8bc8f35c5
MasonMcGough <mcgough.mason@gmail.com>
2018-03-02 14:07:29 -0500

    Made test function to demonstrate delegating function calls via post;

--- app/tools/histotoolkit.py
+++ app/tools/histotoolkit.py
@@ -1,2 +1,8 @@
 def test(data):
     return data
+
+def test_mult(data, arg1):
+    return data * arg1
+
+def test_power(data, arg1):
+    return data**arg1
--- app/views.py
+++ app/views.py
@@ -8,5 +8,26 @@ def home():
 
 @app.route('/test')
 def test():
-	data = 'test data'
-	return htk.test(data)
+    ops_names = ['test_mult', 'test_power']
+    ops_params = [
+        {'arg1': 3},
+        {'arg1': 2}
+    ]
+    ops = [
+        {'op': getattr(htk, op_name),
+         'op_name': op_name,
+         'params': params
+        } for op_name, params in zip(ops_names, ops_params)
+    ]
+
+    val = 2
+    for op in ops:
+        val = op['op'](val, **op['params'])
+
+    return "Result: {0}".format(val)
+
+@app.route('/data-step', methods=['POST'])
+def data_step():
+    # ops_names = request.form['ops_names']
+    # ops_params = request.form['ops_params']
+    return None
