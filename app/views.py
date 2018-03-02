b6cc4057dd58278e7ecd7992dd5fd731d923385c
MasonMcGough <mcgough.mason@gmail.com>
2018-03-02 12:51:35 -0500

    Added histotoolkit.py file and tools package folder;

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import render_template
+from .tools import histotoolkit as htk
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/test')
+def test():
+	data = 'test data'
+	return htk.test(data)
