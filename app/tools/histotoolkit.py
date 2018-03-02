38cef865af5e2637e5e0ad049c33560d3364be1e
MasonMcGough <mcgough.mason@gmail.com>
2018-03-02 16:13:40 -0500

    Implemented count_data_types;

--- app/tools/histotoolkit.py
+++ app/tools/histotoolkit.py
@@ -1,8 +1,18 @@
 import os
+from collections import Counter
+from .PythonHelpers.file_utils import list_files
 
 def count_data_types(data, folder):
-    
-    return {'data-type': ext}
+    all_files = list_files(folder)
+
+    all_exts = []
+    for f in all_files:
+        _, f_ext = os.path.splitext(f)
+        all_exts.append(f_ext)
+
+    counts = Counter(all_exts)
+
+    return counts
 
 
 # TESTING ONLY
--- app/views.py
+++ app/views.py
@@ -3,7 +3,24 @@ from flask import render_template, jsonify
 from .tools import histotoolkit as htk
 import os
 
-data_folder = '/home/masonmcgough/Workspace/HistoToolkit/app/test'
+# data_folder = '/home/masonmcgough/Workspace/HistoToolkit/app/test'
+data_folder = '/home/masonmcgough/Pictures'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data-step', methods=['GET', 'POST'])
+def data_step():
+    # ops_names = request.form['ops_names']
+    # ops_params = request.form['ops_params']
+    ops_names = ['count_data_types']
+    ops_params = [
+        {'folder': data_folder}
+    ]
+
+    img, ops_output = _call_ops(None, ops_names, ops_params)
+    return jsonify(ops_output)
 
 def _call_ops(data, ops_names, ops_params):
     ops = [
@@ -23,23 +40,6 @@ def _call_ops(data, ops_names, ops_params):
 
     return data, ops_output
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/data-step', methods=['GET', 'POST'])
-def data_step():
-    # ops_names = request.form['ops_names']
-    # ops_params = request.form['ops_params']
-    ops_names = ['count_data_types']
-    ops_params = [
-        {'folder': data_folder}
-    ]
-
-    # return htk.get_data_type(ops_names, **)
-    # return _call_ops()
-    img, ops_output = _call_ops(None, ops_names, ops_params)
-    return jsonify(ops_output)
 
 
 # TESTING ONLY
