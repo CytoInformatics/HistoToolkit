55749da35ae19d6d43993a0436e32fb2a876c35a
MasonMcGough <mcgough.mason@gmail.com>
2018-04-18 10:52:10 -0400

    Basic canvas with box and pointer;

--- app/static/js/graph.js
+++ app/static/js/graph.js
@@ -1,13 +1,39 @@
+box_defaults = {
+  strokeColor: 'black',
+  fillColor: 'blue',
+  strokeWidth: 6,
+  point: [200, 200],
+  size: [300, 200]
+};
+
+cursor_defaults = {
+  center: [-100, -100],
+  radius: 5,
+  fillColor: 'black'
+};
+
+function assignProperties(path, properties) {
+  for (key in properties) {
+    path[key] = properties[key];
+  }
+}
+
+// create cursor
+var cursor = new Path.Circle(cursor_defaults);
+cursor.sendToBack();
+function onMouseMove(event) {
+  cursor.position = event.point;
+}
+
 // Create a Paper.js Path to draw a line into it:
-var path = new Path();
+var box = new Path.Rectangle(box_defaults);
 
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-
-// Move to start and draw a line from there
-path.moveTo(start);
-
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+box.onMouseEnter = function(event) {
+  this.previousStrokeColor = this.strokeColor;
+  this.strokeColor = 'green';
+  cursor.visible = false;
+};
+box.onMouseLeave = function(event) {
+  this.strokeColor = this.previousStrokeColor;
+  cursor.visible = true;
+};
