2b2af8f97c431506795ccaa9cbd61d64212167de
MasonMcGough <mcgough.mason@gmail.com>
2018-05-04 16:14:59 -0400

    Remove deprecated _call_ops;

--- app/tools/opnet.py
+++ app/tools/opnet.py
@@ -361,26 +361,3 @@ def ensure_is_listlike(thing):
         thing = [thing,]
 
     return thing
-
-def _call_ops(data, ops_names, ops_params):
-    """
-    Sequentially run DATA through all functions in list OPS_NAMES using the 
-    parameters in list OPS_PARAMS.
-    """
-
-    ops = [
-        {'op': getattr(htk, op_name),
-         'op_name': op_name,
-         'params': params
-        } for op_name, params in zip(ops_names, ops_params)
-    ]
-
-    ops_output = []
-    for op in ops:
-        op_output = op['op'](data, **op['params'])
-        ops_output.append(op_output)
-
-        if 'data' in op_output:
-            data = op_output['data']
-
-    return data, ops_output
